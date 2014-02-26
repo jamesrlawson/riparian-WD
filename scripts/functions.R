09fb8086ae2b5eb2ab3bdc08e8f1192a3e27f258
James Lawson <james.lawson@mq.edu.au>
2014-02-26 16:26:20 +1100

    got rid of all the if else guk in my fitmodels function by adding in a line that subsets x only to vals greater than zero

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,24 +1,8 @@
 fitmodels <- function(x) {
-    
- if(any(hydroCWM$x == 0)) {
-   
-   fit.linear  <- lm(CWM ~ x, data = hydroCWM)
-   fit.quad    <- lm(CWM ~ x + I(x ^2), data = hydroCWM) 
-   
-   linear.pval <- anova(fit.linear)[1,"Pr(>F)"]
-   quad.pval   <- anova(fit.quad)[1,"Pr(>F)"]
-   exp.pval <- c(0)
-   
-   linear.r2   <- summary(fit.linear)$r.squared
-   quad.r2     <- summary(fit.quad)$r.squared
-   exp.r2      <- c(0)  
- }
- 
- else {
-   
+     
   fit.linear  <- lm(CWM ~ x, data = hydroCWM)
   fit.quad    <- lm(CWM ~ x + I(x ^2), data = hydroCWM)  
-  fit.exp     <- lm(CWM ~ x, data = hydroCWM)
+  fit.exp     <- lm(CWM ~ log10(x), subset = x > 0, data = hydroCWM) # THIS IS THE LINE THAT HANDLES INF's!
   
   # p-values
   linear.pval <- anova(fit.linear)[1,"Pr(>F)"]
@@ -30,7 +14,6 @@ fitmodels <- function(x) {
   quad.r2     <- summary(fit.quad)$r.squared
   exp.r2      <- summary(fit.exp)$r.squared  
   
-  }
   
   fitmodels.df <- cbind(linear.pval, linear.r2, quad.pval, quad.r2, exp.pval, exp.r2)
   
